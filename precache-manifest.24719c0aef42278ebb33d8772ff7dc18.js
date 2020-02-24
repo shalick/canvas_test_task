7ff9884e5d4c6d163821e82052fa6e997697fc4b
shalick <alikshabanovich@gmail.com>
2020-02-24 19:16:57 +0300

    Updates

---
+++ precache-manifest.24719c0aef42278ebb33d8772ff7dc18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feba51bf0f3cc90e8f1ae6c430f3fcbb",
+    "url": "/canvas_test_task/index.html"
+  },
+  {
+    "revision": "8db9f5474ca64533a69d",
+    "url": "/canvas_test_task/static/css/main.cf5ec242.chunk.css"
+  },
+  {
+    "revision": "aec28551171132486c63",
+    "url": "/canvas_test_task/static/js/2.6eda02be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/canvas_test_task/static/js/2.6eda02be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db9f5474ca64533a69d",
+    "url": "/canvas_test_task/static/js/main.7d3de730.chunk.js"
+  },
+  {
+    "revision": "2bee56eed991ffd425ec",
+    "url": "/canvas_test_task/static/js/runtime-main.04297658.js"
+  }
+]);
